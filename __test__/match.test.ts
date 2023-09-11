f97b19430fd97421040e336dc2adb54e0d473dd6
wuhonglei <1017368065@qq.com>
2023-09-11 22:52:12 +0800

    fix: 兼容 app 属性路径不存在的情况

--- __test__/match.test.ts
+++ __test__/match.test.ts
@@ -6,6 +6,10 @@ const website = require('../test_data/origin_data/website.json');
 const appPath = path.join(__dirname, '../test_data/origin_data/apps');
 
 function readJsonFile(filePath) {
+  if (!fs.existsSync(filePath)) {
+    return false;
+  }
+
   const fileContent = fs.readFileSync(filePath, 'utf8');
   return JSON.parse(fileContent);
 }
@@ -17,6 +21,10 @@ website.forEach((oneSite) => {
     test(`test ${name} ${oldPath} ${newPath}`, () => {
       const targetProperties = readJsonFile(path.join(appPath, name, 'properties/target.json'));
       const candidateProperties = readJsonFile(path.join(appPath, name, 'properties/candidate.json'));
+      if (!targetProperties || !candidateProperties) {
+        return;
+      }
+
       const targetProperty = findPropertyByXpath(oldPath, targetProperties);
       const candidateProperty = findPropertyByXpath(newPath, candidateProperties);
       expect(targetProperty).not.toBeUndefined();
